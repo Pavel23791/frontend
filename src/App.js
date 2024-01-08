d7108b5240c34081bcee7593ad54b103c17349d9
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2024-01-08 18:55:39 +0200

    first initial

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
+
+const App = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  const loadData = () => {
+    axios.get('http://localhost:5000/api/users')
+    .then(res => {
+      setUsers(res.data.users)
+    })
+    .catch(err => console.log(err.message))
+  }
+
+  return(
+    <div>
+      <ul>
+
+        {
+          users.map(user => <li><b>{user.name}</b> | {user.email}</li>)
+        }
+      </ul>
+    
+    </div>
+  )
+}
+
+export default App;
